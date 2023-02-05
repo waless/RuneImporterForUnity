c84fd5838999b95ee81d2d55355a558878c95998
waless <waless.seel@live.jp>
2023-02-06 01:22:25 +0900

    定数定義クラスを追加し、バージョンを記載

---
+++ RuneImporterForUnity/Assets/RuneImporter/Const.cs
@@ -0,0 +1,14 @@
+
+namespace RuneImporter
+{
+    /// <summary>
+    /// RuneImporterに関する定数定義クラス
+    /// </summary>
+    public static class Const
+    {
+        /// <summary>
+        /// インポータ側のバージョン
+        /// </summary>
+        public const string ImporterVersion = "1.00";
+    }
+}
